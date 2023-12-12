d4d1d9e5596eb903c2d571527937d81b9712458d
tuhinhossain123 <csetuhin55@gmail.com>
2023-12-12 14:33:29 +0600

    website responsive

--- src/Pages/Projects/Projects.jsx
+++ src/Pages/Projects/Projects.jsx
@@ -9,8 +9,11 @@ const Projects = () => {
       <h2 className="text-3xl font-semibold text-center text-white mb-6">
         My Projects
       </h2>
-      <div className="flex justify-center gap-8 max-w-[1200px] mx-auto">
-        <div data-aos="zoom-in-up" className="rounded-b-lg w-[30%] shadow-xl bg-gray-500">
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 max-w-[1200px] mx-auto">
+        <div
+          data-aos="zoom-in-up"
+          className="rounded-b-lg mx-6  shadow-xl bg-gray-500"
+        >
           <img src={img1} alt="" className="rounded-t-lg w-full" />
           <div className="p-4 space-y-4">
             <div className="">
@@ -26,7 +29,7 @@ const Projects = () => {
             <div className="flex gap-3 px-2 ">
               <a
                 className="bg-sky-500 py-1 px-3 text-white rounded-lg flex items-center gap-2"
-                href="https://local-tours-and-guide.web.app/" 
+                href="https://local-tours-and-guide.web.app/"
               >
                 Visit <MdOutlineArrowOutward />
               </a>
@@ -34,32 +37,39 @@ const Projects = () => {
                 className="bg-sky-500 py-1 px-3 text-white rounded-lg flex items-center gap-2"
                 href="https://github.com/Porgramming-Hero-web-course/b8a11-client-side-tuhinhossain123"
               >
-                 Client <FaGithub />
+                Client <FaGithub />
               </a>
               <a
                 className="bg-sky-500 py-1 px-3 text-white rounded-lg flex items-center gap-2"
                 href="https://github.com/Porgramming-Hero-web-course/b8a11-server-side-tuhinhossain123"
               >
-                 Server <FaGithub />
+                Server <FaGithub />
               </a>
             </div>
           </div>
         </div>
-        <div data-aos="zoom-in-up" className="rounded-b-lg w-[30%] shadow-xl bg-gray-500">
+
+        <div
+          data-aos="zoom-in-up"
+          className="rounded-b-lg mx-6  shadow-xl bg-gray-500"
+        >
           <img src={img2} alt="" className="rounded-t-lg w-full" />
           <div className="p-4 space-y-4">
             <div className="">
               <h2 className="text-3xl font-semibold text-white mb-2">
-              NewsPaper
+                NewsPaper
               </h2>
               <p className="text-white">
-              Newspaper project is done with React. Normal user can read news here. If user posts any news, it must be added to admin, otherwise it will not be added. News paper website can be used for free for 1 week. From then on, user can pay to read news.
+                Newspaper project is done with React. Normal user can read news
+                here. If user posts any news, it must be added to admin,
+                otherwise it will not be added. News paper website can be used
+                for free for 1 week. From then on, user can pay to read news.
               </p>
             </div>
             <div className="flex gap-3 px-2 ">
               <a
                 className="bg-sky-500 py-1 px-3 text-white rounded-lg flex items-center gap-2"
-                href="https://newspaper-64536.web.app/" 
+                href="https://newspaper-64536.web.app/"
               >
                 Visit <MdOutlineArrowOutward />
               </a>
@@ -67,23 +77,26 @@ const Projects = () => {
                 className="bg-sky-500 py-1 px-3 text-white rounded-lg flex items-center gap-2"
                 href="https://github.com/programming-hero-web-course1/b8a12-client-side-tuhinhossain123"
               >
-                 Client <FaGithub />
+                Client <FaGithub />
               </a>
               <a
                 className="bg-sky-500 py-1 px-3 text-white rounded-lg flex items-center gap-2"
                 href="https://github.com/programming-hero-web-course1/b8a12-server-side-tuhinhossain123"
               >
-                 Server <FaGithub />
+                Server <FaGithub />
               </a>
             </div>
           </div>
         </div>
-        <div data-aos="zoom-in-up" className="rounded-b-lg w-[30%] shadow-xl bg-gray-500">
+        <div
+          data-aos="zoom-in-up"
+          className="rounded-b-lg mx-6 shadow-xl bg-gray-500"
+        >
           <img src={img3} alt="" className="rounded-t-lg w-full" />
           <div className="p-4 space-y-4">
             <div className="">
               <h2 className="text-3xl font-semibold text-white mb-2">
-              Brand-shop
+                Brand-shop
               </h2>
               <p className="text-white">
                 The offline service sharing web site is a tourist website, where
@@ -94,7 +107,7 @@ const Projects = () => {
             <div className="flex gap-3 px-2 ">
               <a
                 className="bg-sky-500 py-1 px-3 text-white rounded-lg flex items-center gap-2"
-                href="https://fir-recap-6affb.web.app/" 
+                href="https://fir-recap-6affb.web.app/"
               >
                 Visit <MdOutlineArrowOutward />
               </a>
@@ -108,12 +121,11 @@ const Projects = () => {
                 className="bg-sky-500 py-1 px-3 text-white rounded-lg flex items-center gap-2"
                 href="https://github.com/programming-hero-web-course-4/b8a10-brandshop-server-side-tuhinhossain123"
               >
-                 Server <FaGithub />
+                Server <FaGithub />
               </a>
             </div>
           </div>
         </div>
-        
       </div>
     </div>
   );
